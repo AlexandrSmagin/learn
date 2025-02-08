4aa83694f5959d4932fdaac7d23173a9c2b602bf
AlexandrSmagin <alinginzberg@gmail.com>
2025-02-08 14:45:22 +0700

    4lesson

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict'; //пока не совсем врубился зачем это нужно, но ладно
+
+alert( 'Привет, мир!' );
+
+  alert('Привет');
+  alert('Мир'); //это пример двух инструкций в разных строках и комментария к ним
+
+  /* Пример с двумя сообщениями.
+Это - многострочный комментарий.
+*/
+
+// alert( 'не выводится' );
+
+
+let message;
+
+message = 'Hello';
+
+alert (message);
+
+
+
+message = 'Hello';
+
+alert (message);
+
+/*let user = 'Иван';
+let age = 25;
+let message = 'Hello';
+
+
+let user = 'Иван',
+    age = '25',
+    message = 'Hello';*/
+
+
+const NEWYEAR25 = '31.12.24'; //константу нельзя перезаписать
+
+alert (NewYear25);
+
+
+
+let admin, imya;
+
+imya = "Джон"
+
+admin = imya 
+
+alert (admin);
